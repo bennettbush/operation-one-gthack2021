a77b695579816a469444d0df10e1c2279df99fcf
connor16755 <92651687+connor16755@users.noreply.github.com>
2021-10-23 17:19:10 -0400

    Changed chunks to blocks

--- test_allocation_local.py
+++ test_allocation_local.py
@@ -14,12 +14,40 @@ df = pd.DataFrame(data='No Event', index=hourly_index, columns=columns) # initia
 week_start = dt.date(2021, 10, 24) # global variable to declare current_day
 week_end = dt.date(2021, 10, 30)
 
-def test_allocation(event_name, deadline, time_needed, chunks): # for one event; later take in list of "event" objects (really tuples)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# event_name: string representing event_name
+# deadline: string representing date (format: YYYY-MM-DD)
+# time_needed: int representing time_needed to complete assignment/task
+# blocks: bool representing if time_needed must be continuous or could be broken into 1 hour blocks
+def test_allocation(event_name, deadline, time_needed, blocks): # for one event; later take in list of "event" objects (really tuples)
+    
     # function will return list of format [event_name, date, start_time, end_time]
     # looking at weekly frame
-    deadline = get_deadline(deadline) # turns deadline into class datetime.date()
-    number_of_chunks = get_number_of_chunks(time_needed, chunks) # gets number of time slots to split an event amongst
-    list_date_times = get_date_and_times(number_of_chunks, deadline, event_name)
+    
+    deadline = get_deadline(deadline) # turns deadline into class datetime.date() for df
+    number_of_blocks = get_number_of_blocks(time_needed, blocks) # gets number of time slots to split an event amongst
+    list_date_times = get_date_and_times(number_of_blocks, deadline, event_name)
 
     return list_date_times
 
@@ -37,29 +65,29 @@ def get_deadline(deadline): # takes in deadline of type string "YYYY-MM-DD" and 
 
     return deadline
 
-def get_number_of_chunks(time_needed, chunks): # gets number of time slots needed to split an event amongst
-    if chunks == False:
-        number_of_chunks = 1 # allocate all time into one chunk if chunks is not preferred for this event
+def get_number_of_blocks(time_needed, blocks): # gets number of time slots needed to split an event amongst
+    if blocks == False:
+        number_of_blocks = 1 # allocate all time into one chunk if chunks is not preferred for this event
     else:
-        number_of_chunks = time_needed # break all the time_needed into one hour time blocks
+        number_of_blocks = time_needed # break all the time_needed into one hour time blocks
 
-    return number_of_chunks
+    return number_of_blocks
 
-def get_date_and_times(number_of_chunks, deadline, event_name):
+def get_date_and_times(number_of_blocks, deadline, event_name):
     list_date_times = []
     start_day = week_start.day
     end_day = deadline.day
     upper_limit_loop_day = end_day - start_day + 1 # computes the number of days to loop through based off the deadline
     while (number_of_chunks > 0):
         for i in range(upper_limit_loop_day):
-            if number_of_chunks == 0: # catches cases where number_of_chunks goes below 0 during loop
+            if number_of_blocks == 0: # catches cases where number_of_chunks goes below 0 during loop
                 break
             for j in range(len(hourly_index)): # number of hours
                 if df.iloc[i][j] == 'No Event': # if selected timeslot and date aren't filled, populate them with eventName
                     df.iloc[i][j] = event_name # write into the df; avoids overwriting
                     element = [event_name, columns[i], int(hourly_index[j])]
                     list_date_times.append(element)
-                    number_of_chunks -= 1 # decrement number of chunks
+                    number_of_blocks -= 1 # decrement number of chunks
                     break
 
     return list_date_times
