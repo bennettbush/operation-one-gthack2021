9390d6c9e5f2a76342972dfbc154dbb23732c44d
saminampatel <92893817+saminampatel@users.noreply.github.com>
2021-10-23 18:49:18 -0400

    added javascript

---
+++ frontEndJS.js
@@ -0,0 +1,4 @@
+var customerName = prompt("Please enter your name","<name goes here>");
+if (customerName! = null) {
+  document.getElementById("welcome").innerHTML = "Hello" + customerName + "! How are you today?";
+}
