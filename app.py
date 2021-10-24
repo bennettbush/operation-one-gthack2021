dce2e7049f025d9914a73c1f9b728dae34d010a3
smadarapu3 <92894746+smadarapu3@users.noreply.github.com>
2021-10-24 01:42:54 -0400

    integrated UI and flask!!!!

--- app.py
+++ app.py
@@ -8,15 +8,13 @@ app = Flask(__name__)
 app.config["DEBUG"] = True # set to True while debugging
 
 # code goes here
-#@app.route('/frontEnd')
-#def frontEnd():
-    #return render_template('frontEndTest.html')
 
 @app.route('/frontEnd/allocate', methods=['POST']) # link to front end
 def time_allocation():
     req = request.get_data()
     dict_req = wrangle_js_data(req)
-    print(dict_req)
+    arra_req = toArray(dict_req)
+    print(arra_req)
     return '0' # doesn't matter what returns here as what we return to front end doesn't matter
 
 
@@ -30,6 +28,13 @@ def wrangle_js_data(bytes): # turns bytes of data from request.get_data() into c
     string = string[ind1:(ind2+1)]
     dict = json.loads(string) # turns {string} into a dict
     return dict
+
+def toArray(dict): # returns array [string event name, string deadline, integer hours, boolean blocks]
+    int1 = int(dict['hours'])
+    bool1 = bool(dict['blocks'])
+    a1 = [dict['event_name'], dict['deadline'], int1, bool1] # turns dictionary elements to array
+    return a1
+
 
 if __name__ == '__main__':
     app.run()
