d87f4eb3382215232755dae3e24782b5cf09555f
bennettjb12 <bennettjb12@gmail.com>
2021-10-24 01:00:14 -0400

    Working POST Updates

--- app.py
+++ app.py
@@ -15,8 +15,15 @@ app.config["DEBUG"] = True # set to True while debugging
 @app.route('/frontEnd/allocate', methods=['POST']) # link to front end
 def time_allocation():
     req = request.get_data()
-    print(req)
-    print("hello")
+    work2 = str(req)
+    #ind1 = str(req).find('{')
+    #ind2 = str(req).find('}')
+    #work = str(req)[ind1:(ind2 + 1)]
+    # print(work2)
+    ind1 = work2.find('{')
+    ind2 = work2.find('}')
+    work = work2[ind1:(ind2 + 1)]
+    print(work)
     return '0' # doesn't matter what returns here as what we return to front end doesn't matter
 
 
