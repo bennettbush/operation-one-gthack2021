a9d22b08d930c06b8ece2bb26369b2228ec2e281
bennettjb12 <bennettjb12@gmail.com>
2021-10-24 02:00:18 -0400

    Update app.py
    add global calendar variable

--- app.py
+++ app.py
@@ -8,13 +8,18 @@ app = Flask(__name__)
 app.config["DEBUG"] = True # set to True while debugging
 
 # code goes here
+hourly_index = ['09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21'] # hours in military time
+columns = ['2021-10-24', '2021-10-25', '2021-10-26', '2021-10-27', '2021-10-28', '2021-10-29', '2021-10-30'] # weekly time frame
+calendar_df = pd.DataFrame(data='No Event', index=hourly_index, columns=columns) # initializes empty calendar with hourly_index and columns (dates) indices)
+
 
 @app.route('/frontEnd/allocate', methods=['POST']) # link to front end
 def time_allocation():
     req = request.get_data()
-    dict_req = wrangle_js_data(req)
-    arra_req = toArray(dict_req)
+    dict_req = wrangle_js_data(req) # turns POST request data into a dictionary
+    arra_req = to_array(dict_req) # turns dictionary into valid input for allocation
     print(arra_req) # comment this out later
+    print(calendar_df)
     return '0' # doesn't matter what returns here as what we return to front end doesn't matter
 
 
@@ -29,7 +34,7 @@ def wrangle_js_data(bytes): # turns bytes of data from request.get_data() into c
     dict = json.loads(string) # turns {string} into a dict
     return dict
 
-def toArray(dict): # returns array [string event name, string deadline, integer hours, boolean blocks]
+def to_array(dict): # returns array [string event name, string deadline, integer hours, boolean blocks]
     int1 = int(dict['hours'])
     bool1 = bool(dict['blocks'])
     a1 = [dict['event_name'], dict['deadline'], int1, bool1] # turns dictionary elements to array
